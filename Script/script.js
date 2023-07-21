21abc7a070ffb427496ea1c2f645387369dbb6df
Michael Hannah <Michaelrkhannah@gmail.com>
2023-07-20 18:21:48 -0600

    Added for loop and if statments

--- Script/script.js
+++ Script/script.js
@@ -1,14 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lCAlpha= "abcdefghijklmnopqrstuvwxyz";
-var uCAlpha= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var uCAlpha= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers= "1234567890";
 var characters= "!@#$%^&*()";
 
 // Write password to the #password input
 function generatePassword(){
   var length=prompt("Enter number of characters for password.")
-  console.log(length)
   if (length < 8 || length > 128 || isNaN(length)) {
     alert("Please enter a numerical value between 8 and 128.")
     generatePassword()
@@ -17,10 +16,20 @@ function generatePassword(){
   var uppercase= confirm("Do you want uppercase letters included in password?")
   var numerical= confirm("Do you want numbers included in password?")
   var specialCharacters= confirm("Do you want special characters included in password?")
-
+  
+  let passW= "";
+ for (var i=0; i < length/4; i++) {
   if (lowercase){
-    return lowercase[Math.floor(Math.random() * lCAlpha.length)]
+    passW+= lCAlpha[Math.floor(Math.random() * lCAlpha.length)]
+  }if (uppercase){
+    passW+= uCAlpha[Math.floor(Math.random() * uCAlpha.length)]
+  }if (numerical){
+    passW += numbers[Math.floor(Math.random() * numbers.length)]
+  }if (specialCharacters){
+    passW += characters[Math.floor(Math.random() * characters.length)]
   }
+}
+return passW
 }
 
 function writePassword() {
